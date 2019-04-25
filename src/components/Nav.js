82ca342219e6eb7462a055ba6d75b677b350dfa2
Alex Kale <hi@awkale.me>
2019-04-25 15:52:08 -0400

    add logo to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+import logo from '../images/AgentA-red-transparent.png';
+
+const Navbar = styled.div`
+  display: flex;
+
+  a {
+    color: #fff;
+  }
+`;
+
+const NavLogo = styled.div`
+  width: 130px;
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
 
 const Nav = () => (
-  <div className="nav-main">
+  <Navbar>
+    <NavLogo>
+      <img src={logo} alt="Agent A logo" />
+    </NavLogo>
     <Link to="/process">Process</Link>
     <Link to="/work">Work</Link>
     <Link to="/agents">The Agents</Link>
-  </div>
+  </Navbar>
 );
 
 export default Nav;
