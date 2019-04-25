37be347d6fcbd7f01474e5a8df13f0a52ca9cebe
Alex Kale <hi@awkale.me>
2019-04-25 12:15:13 -0400

    implement typist

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,17 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 import SEO from '../components/Seo';
+import Typist from 'react-typist';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`Agent A`, `brand innovation`, `react`]} />
-    <h1>Agent A is a brand innovation studio</h1>
-    <p>
-      Agent A is a brand innovation studio We merge curiosity, insight and
-      imagination to help brands matter.{' '}
-    </p>
+    <Typist>
+      <h1>Agent A is a brand innovation studio</h1>
+      <h2>
+        We merge curiosity, insight and imagination to help brands matter.
+      </h2>
+    </Typist>
     <Link to="/process/">Process</Link>
   </Layout>
 );
