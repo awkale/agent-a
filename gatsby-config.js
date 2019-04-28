21710bf1b4200c8ee23c6b2ed105b8fb84e4013e
Alex Kale <hi@awkale.me>
2019-04-27 20:32:41 -0400

    linting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title: `Agent A`,
-    description: `Agent A is a brand innovation studio. We merge curiosity, insight and imagination to help brands matter. `,
-    author: `@awkale`,
+    title: 'Agent A',
+    description: 'Agent A is a brand innovation studio. We merge curiosity, insight and imagination to help brands matter. ',
+    author: '@awkale',
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-styled-components`,
+      resolve: 'gatsby-plugin-styled-components',
       options: {
         // Add any options here
       },
     },
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Agent A`,
-        short_name: `Agent A`,
-        start_url: `/`,
-        background_color: `#d22630`,
-        theme_color: `#d22630`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        name: 'Agent A',
+        short_name: 'Agent A',
+        start_url: '/',
+        background_color: '#d22630',
+        theme_color: '#d22630',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`,
+    'gatsby-plugin-sass',
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
