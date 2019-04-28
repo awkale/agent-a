e4067b1f28c6c10c9d131db76d2d55cad3ffebc7
Alex Kale <hi@awkale.me>
2019-04-27 20:31:51 -0400

    update typist settings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import Typist from 'react-typist';
 import Layout from '../components/Layout';
 import SEO from '../components/Seo';
-import Typist from 'react-typist';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`Agent A`, `brand innovation`, `react`]} />
+    <SEO title="Home" keywords={['Agent A', 'brand innovation', 'react']} />
     <div className="typist-wrapper">
-      <Typist>
+      <Typist cursor={{ show: false }}>
         <h1 className="typist-heading">Agent A is a brand innovation studio</h1>
         <h2 className="typist-heading">
           We merge curiosity, insight and imagination to help brands matter.
