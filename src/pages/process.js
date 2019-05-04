18af13755145d565ffb88d92f645d33b34adfa8c
Alex Kale <hi@awkale.me>
2019-05-04 17:15:05 -0400

    add graphql query to contentful

--- src/pages/process.js
+++ src/pages/process.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
+import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/Seo';
 
-const Process = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Process</h1>
-    <p>process page</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-);
+const Process = ({ data }) => {
+  const processes = data.allContentfulProcess.edges;
+
+  return (
+    <Layout>
+      <SEO title="Process" />
+      <h1>How We Do It</h1>
+      {processes.map(process => (
+        <div>
+          <h2>{process.node.processType}</h2>
+          {process.node.processTypeDescription.content.map(text => (
+            <p>{text.content[0].value}</p>
+          ))}
+        </div>
+      ))}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ProcessQuery {
+    allContentfulProcess {
+      edges {
+        node {
+          processType
+          processTypeDescription {
+            id
+            content {
+              nodeType
+              content {
+                value
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Process;
