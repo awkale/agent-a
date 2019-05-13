1864592993732de6a8ab3bb29766b37923bbfe59
Alex Kale <hi@awkale.me>
2019-05-13 18:24:40 -0400

    change columns

--- src/components/styles/ImageGrid.js
+++ src/components/styles/ImageGrid.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ImageGrid = styled.div`
   display: grid;
   grid-gap: 0 40px;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   margin-bottom: 4rem;
 `;
 
