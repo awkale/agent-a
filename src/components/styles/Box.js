96d15d1375dcbb8eeb333bf7e0b8c63ed559ab06
Alex Kale <hi@awkale.me>
2019-05-13 17:16:27 -0400

    create box component

---
+++ src/components/styles/Box.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+  background: linear-gradient(
+    to top,
+    ${props => props.theme.gray900} 50%,
+    ${props => props.theme.red} 50%
+  );
+  background-size: 100% 200%;
+  background-position: bottom left;
+  padding: 20px;
+  transition: all 0.2s ease;
+
+  p {
+    color: #fff;
+    visibility: hidden;
+  }
+
+  :hover {
+    background-position: top left;
+    p {
+      visibility: visible;
+    }
+  }
+`;
+
+export default Box;
