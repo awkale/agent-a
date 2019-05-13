ba21815d1f773b4dd7e163d9cf3694003615327c
Alex Kale <hi@awkale.me>
2019-05-13 17:16:11 -0400

    create image grid component

---
+++ src/components/styles/ImageGrid.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const ImageGrid = styled.div`
+  display: grid;
+  grid-gap: 40px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`;
+
+export default ImageGrid;
