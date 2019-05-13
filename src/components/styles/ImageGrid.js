7891c42995f9284ab3b568ae94595766b82e121a
Alex Kale <hi@awkale.me>
2019-05-13 17:20:40 -0400

    adjust grid styles

--- src/components/styles/ImageGrid.js
+++ src/components/styles/ImageGrid.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 const ImageGrid = styled.div`
   display: grid;
-  grid-gap: 40px;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 0 40px;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  margin-bottom: 4rem;
 `;
 
 export default ImageGrid;
