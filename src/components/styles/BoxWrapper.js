5ac752b0d1b263ed159d35d59e9f5e4255da76a5
Alex Kale <hi@awkale.me>
2019-05-13 17:16:20 -0400

    create box wrapper component

---
+++ src/components/styles/BoxWrapper.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const BoxWrapper = styled.div`
+  display: grid;
+  grid-gap: 40px;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+export default BoxWrapper;
