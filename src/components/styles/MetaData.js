93a41e5e7f07e2d85d849d070382957180c2f26a
Alex Kale <hi@awkale.me>
2019-05-13 17:16:00 -0400

    create meta component

---
+++ src/components/styles/MetaData.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const MetaData = styled.h3`
+  font-size: 0.8rem;
+  text-transform: uppercase;
+  line-height: 1;
+  margin-bottom: 20px;
+  color: ${props => props.theme.red};
+`;
+
+export default MetaData;
