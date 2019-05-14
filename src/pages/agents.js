6ac66723cbf09479bbaed4649f503f5dd53c6b88
Alex Kale <hi@awkale.me>
2019-05-14 15:30:29 -0400

    create agents page

---
+++ src/pages/agents.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '../components/Layout';
+import SEO from '../components/Seo';
+import PageTitle from '../components/styles/PageTitle';
+import PageSubTitle from '../components/styles/PageSubTitle';
+
+const Agents = () => (
+  <Layout>
+    <SEO title="Agents" />
+    <PageTitle>Agents</PageTitle>
+  </Layout>
+);
+
+export default Agents;
