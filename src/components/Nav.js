aa8150b5a302a89e7b2313230d3fd1805b61132c
Alex Kale <hi@awkale.me>
2019-05-15 22:53:14 -0400

    start mobile nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,6 +7,31 @@ const Navbar = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 5rem;
+  @media (max-width: ${props => props.theme.sm}) {
+    flex-direction: column;
+  }
+`;
+
+const ToggleButton = styled.div`
+  padding: 4px 12px;
+  display: none;
+  align-self: flex-end;
+  position: absolute;
+
+  @media (max-width: ${props => props.theme.sm}) {
+    display: block;
+  }
+`;
+
+const ToggleIcon = styled.span`
+  display: inline-block;
+  width: 1.5em;
+  height: 1.5em;
+  vertical-align: middle;
+  content: '';
+  background: no-repeat center center;
+  background-size: 100% 100%;
+  background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3e%3cpath stroke='rgba(0, 0, 0, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
 `;
 
 const NavLinkWrapper = styled.ul`
@@ -14,6 +39,11 @@ const NavLinkWrapper = styled.ul`
   display: flex;
   align-items: center;
   margin-bottom: 0;
+
+  @media (max-width: ${props => props.theme.sm}) {
+    flex-direction: column;
+    padding: 10px 0;
+  }
 `;
 
 const NavLink = styled.li`
@@ -21,6 +51,10 @@ const NavLink = styled.li`
   letter-spacing: 1px;
   font-size: 1rem;
   line-height: 1rem;
+
+  @media (max-width: ${props => props.theme.sm}) {
+    padding: 10px 0;
+  }
 
   a {
     color: #fff;
@@ -53,6 +87,9 @@ const Nav = () => (
         <img src={logo} alt="Agent A logo" />
       </Link>
     </NavLogo>
+    <ToggleButton>
+      <ToggleIcon />
+    </ToggleButton>
     <NavLinkWrapper>
       <NavLink>
         <Link activeClassName="active" to="/what-we-do">
