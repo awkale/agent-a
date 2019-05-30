9213a58fa4e387b86cc8297458d44e2caf4fac3e
Alex Kale <hi@awkale.me>
2019-05-30 10:00:15 -0400

    switch to gtag and remove GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
   throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
+    'Contentful spaceId and the delivery token need to be provided.',
   );
 }
 
@@ -30,26 +30,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: 'gatsby-plugin-gtag',
       options: {
+        // your google analytics tracking id
         trackingId: 'UA-171213-17',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        // head: true,
-        // Setting this parameter is optional
-        // anonymize: true,
-        // Setting this parameter is also optional
-        // respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Enables Google Optimize using your container Id
-        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Enables Google Optimize Experiment ID
-        // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-        // Any additional create only fields (optional)
-        // siteSpeedSampleRate: 10,
-        // cookieDomain: 'agent-a-ny.com',
+        // Puts tracking script in the head instead of the body
+        head: true,
       },
     },
     {
