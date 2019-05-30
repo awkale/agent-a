763b5bd0a8dfb5f446766346b1b0ed35fc24c6d5
Alex Kale <hi@awkale.me>
2019-05-30 09:10:28 -0400

    update ga settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,9 +36,9 @@ module.exports = {
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
-        anonymize: true,
+        // anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        // respectDNT: true,
         // Avoids sending pageview hits from custom paths
         // exclude: ['/preview/**', '/do-not-track/me/too/'],
         // Enables Google Optimize using your container Id
@@ -48,8 +48,7 @@ module.exports = {
         // Set Variation ID. 0 for original 1,2,3....
         // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
         // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
+        // siteSpeedSampleRate: 10,
         cookieDomain: 'agent-a-ny.com',
       },
     },
