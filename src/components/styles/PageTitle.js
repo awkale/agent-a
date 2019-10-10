1c4ded9ac429e748aaafae6b6e95700be190bdac
Alex Kale <hi@awkale.me>
2019-10-09 23:47:05 -0400

    tweak typography

--- src/components/styles/PageTitle.js
+++ src/components/styles/PageTitle.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 const PageTitle = styled.h1`
-  font-size: 2.5rem;
+  font-size: 4rem;
   margin-bottom: 3rem;
-  text-align: center;
   color: #fff;
-  padding-left: 10%;
   padding-right: 10%;
+  letter-spacing: -2px;
+  font-weight: normal;
 `;
 
 export default PageTitle;
