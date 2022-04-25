45a47d1f426dd97d48d2a9f557fdf62808bb1252
Alex Kale <akale@dv01.co>
2022-04-24 22:02:33 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,88 @@
-process.chdir(__dirname);
-
-module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    allowImportExportEverywhere: true,
-    codeFrame: false,
+module.exports = exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
   },
-  extends: ['airbnb-bundle'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'jest'],
   rules: {
-    'react/jsx-filename-extension': [
-      1,
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': ['warn'],
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-use-before-define': ['warn'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        extensions: ['.js', '.jsx'],
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
       },
     ],
+    'max-len': [
+      'warn',
+      {
+        code: 120,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreUrls: true,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'warn',
+    'no-case-declarations': 'off',
+    'no-plusplus': 'off',
+    'no-return-await': 'off',
+    'no-shadow': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'react/jsx-props-no-spreading': 'warn',
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/no-array-index-key': 'warn',
+    camelcase: 'warn',
+    'import/named': 'warn',
+  },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['warn'],
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'frontend/'],
+      },
+    },
   },
 };
