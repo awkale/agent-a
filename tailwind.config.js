e4ff19f21d1e0df224f201bfc386fc8232d54afd
Alex Kale <akale@dv01.co>
2022-04-24 22:40:40 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'red': '#d22630',
+      'coolGray1c': '#d9d9d6',
+      'coolGray6c': '#a7a8aa',
+      'coolGray11c': '#53565A',
+      'gray900': '#212529',
+    },
     fontFamily: {
       'display': ['noway_roundregular', 'sans-serif'],
       'body': ['Univers LT Std 55 Roman', 'sans-serif'],
