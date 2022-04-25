057be1c3532405bcbd0f5322c46cd6f9b770caaf
Alex Kale <akale@dv01.co>
2022-04-24 22:03:17 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'display': ['noway_roundregular', 'sans-serif'],
+      'body': ['Univers LT Std 55 Roman', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
