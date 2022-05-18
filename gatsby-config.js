aa745343f5ff347b418fe1b28d2b5f1b7c4bd7eb
Alex Kale <akale@dv01.co>
2022-05-17 23:44:18 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ let contentfulConfig;
 try {
   // Load the Contentful config from the .contentful.json
   contentfulConfig = require('./.contentful');
-} catch (_) {}
+} catch (_) { }
 
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
@@ -100,7 +100,6 @@ module.exports = {
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-sass',
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
