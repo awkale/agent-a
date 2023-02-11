ecde459daba276a1506804aa2bcb0e5130b29d0a
Alex Kale <hi@awkale.me>
2023-02-11 18:15:06 -0500

    Update work.tsx

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -5,10 +5,6 @@ import React from 'react'
 import Helmut from 'react-helmet'
 import Layout from '../components/Layout'
 import SEO from '../components/Seo'
-import BoxWrapper from '../components/styles/BoxWrapper'
-import ImageGrid from '../components/styles/ImageGrid'
-import PageSubTitle from '../components/styles/PageSubTitle'
-import PageTitleDivider from '../components/styles/PageTitleDivider'
 
 const WorkPage = ({ data }: PageProps<Queries.WorkPageQuery>) => (
   <Layout>
@@ -21,26 +17,30 @@ const WorkPage = ({ data }: PageProps<Queries.WorkPageQuery>) => (
     <h1 className="text-white text-5xl md:text-8xl tracking-tight mb-10 md:mb-24">
       Missions Accomplished
     </h1>
-    <BoxWrapper>
+    <div className="grid gap-x-12 gap-y-20 md:grid-cols-3">
       {data.allContentfulWork.edges.map((work) => (
         <div className="p-5">
           <h3 className="text-xs text-red uppercase mb-5">
             {work.node.category.category}
           </h3>
-          <PageSubTitle>{work.node.clientName}</PageSubTitle>
+          <h2 className="text-4xl leading-none text-white mb-8 after:content-[''] after:block after:w-[45px] after:h-[5px] after:mt-5 after:bg-red">
+            {work.node.clientName}
+          </h2>
           <p className="text-xl">{work.node.description.description}</p>
         </div>
       ))}
-    </BoxWrapper>
-    <PageTitleDivider>Brands we've helped</PageTitleDivider>
-    <ImageGrid>
+    </div>
+    <h2 className="text-3xl leading-none text-white mt-48 text-center relative ">
+      Brands we've helped
+    </h2>
+    <div className="w-[70%] my-0 mx-auto grid gap-16 mt-16 mb-32 grid-cols-2 md:grid-cols-5">
       {data.allFile.edges.map((image) => (
         <GatsbyImage
           key={image.node.id}
           image={image.node.childImageSharp.gatsbyImageData}
         />
       ))}
-    </ImageGrid>
+    </div>
   </Layout>
 )
 
