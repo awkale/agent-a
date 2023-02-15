d4fea39df474bd017169b8227e7f5f760f79a470
Alex Kale <hi@awkale.me>
2023-02-14 23:32:19 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Nav from './Nav';
+import * as React from 'react'
+import Nav from './Nav'
 
-const Header = ({ siteTitle }) => (
+export const Header = ({}) => (
   <header>
     <div>
       <Nav />
     </div>
   </header>
-);
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-  siteTitle: '',
-};
-
-export default Header;
+)
