35f35d68067bc2b78741f359c49fd9b707340293
Alex Kale <hi@awkale.me>
2023-02-14 22:18:36 -0500

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import { HeadProps, Link, PageProps } from 'gatsby'
+import React from 'react'
+import { SEO } from '../components'
+import Layout from '../components/Layout'
+import { SeoProps } from '../types'
 
-import Layout from '../components/Layout';
-import SEO from '../components/Seo';
-
-const NotFoundPage = () => (
+const NotFoundPage = (props: PageProps) => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className="text-white text-5xl md:text-8xl tracking-tight mb-10 md:mb-24">
+      Page Not Found
+    </h1>
+    <p className="text-xl mb-8">
+      You just hit a route that doesn&#39;t exist... the sadness.
+    </p>
+    <p>
+      <Link className="text-xl hover:text-red" to="/">
+        Return home
+      </Link>
+    </p>
   </Layout>
-);
+)
 
-export default NotFoundPage;
+export default NotFoundPage
+
+export const Head = ({}: HeadProps<SeoProps>) => (
+  <>
+    <body className="not-found" />
+    <SEO title="404: Not found" />
+  </>
+)
