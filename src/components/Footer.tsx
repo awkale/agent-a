118a6ca8465d983e5fedbace6661a2358ba68455
Alex Kale <hi@awkale.me>
2023-02-14 23:32:14 -0500

    remove styled components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,51 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-import { OutboundLink } from 'gatsby-plugin-google-gtag'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faInstagram,
-  faLinkedin,
-  faTwitter,
-} from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faAt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { OutboundLink } from 'gatsby-plugin-google-gtag'
+import * as React from 'react'
 
-const FooterNav = styled.ul`
-  list-style: none;
-  display: flex;
-  padding-left: 0;
-`
-
-const FooterNavLink = styled.li`
-  padding-top: 10px;
-  padding-right: 15px;
-  padding-bottom: 10px;
-
-  a {
-    color: #fff;
-
-    &:hover {
-      color: #53565a;
-    }
-  }
-`
-
-const Copyright = styled.div`
-  color: #53565a;
-  text-align: right;
-`
-
-function Footer() {
+export function Footer() {
   return (
     <footer className="px-4">
-      <FooterNav>
-        <FooterNavLink>
-          <OutboundLink href="mailto:alex@agent-a-ny.com">
+      <ul className="list-none flex pl-0">
+        <li className="pt-2 pr-4 pb-3">
+          <OutboundLink
+            className="text-white hover:text-gray-500"
+            href="mailto:alex@agent-a-ny.com"
+          >
             <FontAwesomeIcon size="lg" icon={faAt} />
             <span className="sr-only">email</span>
           </OutboundLink>
-        </FooterNavLink>
-        <FooterNavLink>
+        </li>
+        <li className="pt-2 pr-4 pb-3">
           <OutboundLink
+            className="text-white hover:text-gray-500"
             href="https://www.instagram.com/agent_a_ny/"
             target="_blank"
             rel="noopener noreferrer"
@@ -53,9 +27,10 @@ function Footer() {
             <FontAwesomeIcon size="lg" icon={faInstagram} />
             <span className="sr-only">Instagram</span>
           </OutboundLink>
-        </FooterNavLink>
-        <FooterNavLink>
+        </li>
+        <li className="pt-2 pr-4 pb-3">
           <OutboundLink
+            className="text-white hover:text-gray-500"
             href="https://www.linkedin.com/in/alejandro-rodriguez-b406ab4/"
             target="_blank"
             rel="noopener noreferrer"
@@ -63,11 +38,9 @@ function Footer() {
             <FontAwesomeIcon size="lg" icon={faLinkedin} />
             <span className="sr-only">Linkedin</span>
           </OutboundLink>
-        </FooterNavLink>
-      </FooterNav>
-      <Copyright>{`© Agent A ${new Date().getFullYear()}`}</Copyright>
+        </li>
+      </ul>
+      <div className="text-gray-500 text-right">{`© Agent A ${new Date().getFullYear()}`}</div>
     </footer>
   )
 }
-
-export default Footer
