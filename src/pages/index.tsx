62629f53746b4849ad372a31301e804f8554d1f2
Alex Kale <hi@awkale.me>
2023-02-14 23:48:06 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,35 @@ function IndexPage({ data }: PageProps<Queries.IndexPageQuery>) {
   return (
     <Layout>
       <div className="grid grid-cols-12 grid-rows-6 ">
-        <h1 className="text-white leading-tight text-5xl">
-          We’re a multi-disciplinary brand innovation team
+        <h1 className="col-start-3 col-span-9 row-start-2 text-red leading-tight text-5xl">
+          Agent A is a Brooklyn-based brand and innovation studio.
         </h1>
-        <h2 className="text-white leading-tight text-7xl">Truth</h2>
-        <h2 className="text-white leading-tight text-7xl">Ideas</h2>
-        <h2 className="text-white leading-tight text-7xl">Courage</h2>
+        <h1 className="col-start-3 col-span-9 row-start-4 text-red leading-tight text-5xl">
+          We’re multicultural moderators, researchers, brand strategists,
+          creatives & designers working with some of the world’s leading brands.
+        </h1>
+        <h1 className="col-start-3 col-span-9 row-start-6 text-red leading-tight text-5xl">
+          We merge consumer & cultural insights with imagination, curiosity and
+          a healthy obsession with facts to unlock growth.
+        </h1>
+        <h2 className="col-start-1 row-start-1 text-red leading-tight text-9xl font-extrabold">
+          Truth
+        </h2>
+        <h2 className="col-start-1 row-start-1 translate-y-6 -translate-x-6 text-black leading-tight text-9xl font-extrabold">
+          Truth
+        </h2>
+        <h2 className="col-start-1 row-start-3 text-red leading-tight text-9xl font-extrabold">
+          Ideas
+        </h2>
+        <h2 className="col-start-1 row-start-3 translate-y-6 -translate-x-6 text-black leading-tight text-9xl font-extrabold">
+          Ideas
+        </h2>
+        <h2 className="col-start-1 row-start-5 text-red leading-tight text-9xl font-extrabold">
+          Courage
+        </h2>
+        <h2 className="col-start-1 row-start-5 translate-y-6 -translate-x-6 text-black leading-tight text-9xl font-extrabold">
+          Courage
+        </h2>
       </div>
     </Layout>
   )
