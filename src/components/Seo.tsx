fbca61b0353d663e7530432b3a6956a22d4f2b01
Alex Kale <hi@awkale.me>
2023-02-14 22:59:18 -0500

    Update Seo.tsx

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -31,10 +31,8 @@ export const SEO = ({
   return (
     <>
       <meta charSet="utf-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
-      />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>{seo.title}</title>
       <meta name="author" content="dv01" />
       <meta name="keywords" content={seo.keywords} />
