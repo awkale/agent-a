1b5c00ab01887d59ce3e9d4ea1c9dce1aac44afa
Alex Kale <hi@awkale.me>
2023-02-15 12:10:07 -0500

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,93 +1,10 @@
-import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'gatsby'
-import styled from 'styled-components'
+import React, { Component } from 'react'
+import { classNames } from '../hooks/classNames'
 import logo from '../images/AgentA-red-transparent.png'
 import '../styles/nav.css'
-
-const Navbar = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 5rem;
-  @media (max-width: 576px) {
-    flex-direction: column;
-    margin-bottom: 1rem;
-    align-items: flex-start;
-  }
-`
-
-const ToggleButton = styled.div`
-  padding: 4px 12px;
-  display: none;
-  align-self: flex-end;
-  position: absolute;
-  top: 10px;
-
-  @media (max-width: 576px) {
-    display: block;
-  }
-`
-
-const ToggleIcon = styled.span`
-  display: inline-block;
-  width: 1.5em;
-  height: 1.5em;
-  vertical-align: middle;
-  content: '';
-  background: no-repeat center center;
-  background-size: 100% 100%;
-  background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3e%3cpath stroke='rgba(250, 250, 250, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
-`
-
-const NavLinkWrapper = styled.ul`
-  list-style: none;
-  display: flex;
-  align-items: center;
-  margin-bottom: 0;
-
-  padding: 0;
-  height: 24px;
-  overflow: hidden;
-
-  @media (max-width: 576px) {
-    height: auto;
-    flex-direction: column;
-    padding: 10px 0;
-    display: ${(props) => (props.isNavShown ? 'flex' : 'none')};
-  }
-`
-
-const NavLink = styled.li`
-  font-family: n27regular;
-  letter-spacing: 1px;
-  font-size: 1rem;
-
-  @media (max-width: 576px) {
-    padding: 0;
-    width: 100%;
-  }
-
-  a {
-    text-transform: uppercase;
-    padding: 4px 15px 2px;
-    margin-left: 30px;
-
-    @media (max-width: 576px) {
-      padding: 10px 10px 7px;
-      margin-left: 10px;
-      display: block;
-    }
-  }
-`
-
-const NavLogo = styled.div`
-  width: 130px;
-
-  img {
-    max-width: 100%;
-    height: auto;
-  }
-`
 
 class Nav extends Component {
   constructor(props) {
@@ -106,37 +23,37 @@ class Nav extends Component {
 
   render() {
     return (
-      <Navbar>
-        <NavLogo>
+      <nav className="flex flex-col md:flex-row justify-between items-start md:items-center mb-4 md:mb-20 ">
+        <div className="w-32 ">
           <Link to="/">
-            <img src={logo} alt="Agent A logo" />
+            <img className="max-w-full h-auto" src={logo} alt="Agent A logo" />
           </Link>
-        </NavLogo>
-        <ToggleButton onClick={this.toggleNav}>
-          <ToggleIcon />
-        </ToggleButton>
-        <NavLinkWrapper isNavShown={this.state.hamburgerIsOpen}>
-          <NavLink>
+        </div>
+        <div className="py-1 px-3 block md:hidden self-end absolute top-[10px]" onClick={this.toggleNav}>
+          <FontAwesomeIcon icon={faBars} className="w-6 h-6 " />
+        </div>
+        <ul className={classNames("list-none flex flex-col md:flex-row items-center mb-0 py-[10px] px-0 md:p-0 h-auto md:h-6 overflow-hidden", this.state.hamburgerIsOpen ? 'flex' : 'none')} >
+          <li className="font-brand tracking-wider text-base">
             <Link
-              className="nav-hover"
+              className="nav-hover block md:inline uppercase p-2 md:pt-1 md:pb-[2px] md:px-4 ml-2 md:ml-7"
               activeClassName="active"
               to="/what-we-do"
             >
               What We Do
             </Link>
-          </NavLink>
-          <NavLink>
-            <Link className="nav-hover" activeClassName="active" to="/work">
+          </li>
+          <li className="font-brand tracking-wider text-base">
+            <Link className="nav-hover block md:inline uppercase p-2 md:pt-1 md:pb-[2px] md:px-4 ml-2 md:ml-7" activeClassName="active" to="/work">
               Work
             </Link>
-          </NavLink>
-          <NavLink>
-            <Link className="nav-hover" activeClassName="active" to="/agents">
+          </li>
+          <li className="font-brand tracking-wider text-base">
+            <Link className="nav-hover block md:inline uppercase p-2 md:pt-1 md:pb-[2px] md:px-4 ml-2 md:ml-7" activeClassName="active" to="/agents">
               The Agents
             </Link>
-          </NavLink>
-        </NavLinkWrapper>
-      </Navbar>
+          </li>
+        </ul>
+      </nav>
     )
   }
 }
