f8eda1b6fcf765b781e6e1db6358bd76fbeeaaf8
Alex Kale <hi@awkale.me>
2025-04-22 13:22:18 -0400

    add playlist

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ import * as React from 'react'
 export function Footer() {
   const data = useStaticQuery(graphql`
     query FooterQuery {
-      contentfulAsset(contentful_id: {eq: "3T1W24dQzYEcbrJ9h5GIGR"}) {
+      contentfulAsset(contentful_id: { eq: "3T1W24dQzYEcbrJ9h5GIGR" }) {
         id
         file {
           url
@@ -22,10 +22,18 @@ export function Footer() {
       <div className="container mx-auto">
         <div className="grid grid-cols-12 mb-16">
           <ul className="list-none col-span-4 md:col-span-3 col-start-3 md:col-start-1 flex flex-col pl-0 text-xl md:text-3xl font-brand text-gray-900 ">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/what-we-do">What We Do</Link></li>
-            <li><Link to="/work">Work</Link></li>
-            <li><Link to="/agents">Agents</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/what-we-do">What We Do</Link>
+            </li>
+            <li>
+              <Link to="/work">Work</Link>
+            </li>
+            <li>
+              <Link to="/agents">Agents</Link>
+            </li>
           </ul>
           <ul className="list-none col-span-4 md:col-span-3 col-start-8 md:col-start-4 flex flex-col pl-0 text-xl md:text-3xl font-brand text-gray-900 ">
             <li className="mb-2">
@@ -46,13 +54,27 @@ export function Footer() {
                 Linkedin <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
               </OutboundLink>
             </li>
-
           </ul>
+          <div className="col-span-full md:col-span-6">
+            <iframe
+              title="Apple Music"
+              allow="autoplay *; encrypted-media *;"
+              height="450"
+              className="w-full max-w-2xl overflow-hidden bg-transparent"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+              src="https://embed.music.apple.com/us/playlist/agent-a-radio/pl.u-leyl82AUj8Kj3m"
+            />
+          </div>
         </div>
-
       </div>
       <div className="pt-2 pr-4 pb-3">
-        <object title={data.contentfulAsset.title} data={data.contentfulAsset.file.url} type="image/svg+xml">© Agent A</object>
+        <object
+          title={data.contentfulAsset.title}
+          data={data.contentfulAsset.file.url}
+          type="image/svg+xml"
+        >
+          © Agent A
+        </object>
       </div>
     </footer>
   )
